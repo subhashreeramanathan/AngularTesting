e5509982ef81fe84c148d2e86fb8457b43d00db5
subhashreeramanathan <subhashree.ramanathan@syncfusion.com>
2017-11-07 10:05:52 +0530

    Add files via upload

---
+++ config/karma.conf.js
@@ -0,0 +1,47 @@
+var webpackConfig = require('./webpack.test');
+
+module.exports = function (config) {
+  var _config = {
+    basePath: '',
+
+    frameworks: ['jasmine'],
+
+    files: [
+      {pattern: './config/karma-test-shim.js', watched: false}
+      
+    ],
+
+    preprocessors: {
+      './config/karma-test-shim.js': ['webpack', 'sourcemap']
+    },
+
+    webpack: webpackConfig,
+
+    webpackMiddleware: {
+      stats: 'errors-only'
+    },
+
+    webpackServer: {
+      noInfo: true
+    },
+
+    reporters: ['progress', 'kjhtml', 'html'],
+    htmlReporter: {
+      outputFile: 'report/report.html',
+
+      // Optional
+      groupSuites: true,
+      useCompactStyle: true,
+      useLegacyStyle: true
+    },
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: [ 'Chrome', 'Firefox', 'Edge'],
+    concurrency: 1,
+    singleRun: true
+  };
+
+  config.set(_config);
+};
